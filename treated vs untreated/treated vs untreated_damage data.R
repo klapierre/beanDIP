762d4140e7387c647eb8a8a3ed7bbc70b29f1096
klapierre <kjkomatsu@uncg.edu>
2023-01-10 16:24:47 -0500

    Importing data.

--- treated vs untreated/treated vs untreated_damage data.R
+++ treated vs untreated/treated vs untreated_damage data.R
@@ -1,2 +1,20 @@
-#####
-# treated vs untreated damage data.R: script to examine insect damage
+################################################################################
+##  treated vs untreated_damage data.R: Examining field soybean trial insect damage data.
+##
+##  Authors: Kimberly Komatsu
+##  Date created: January 10, 2023
+################################################################################
+
+library(tidyverse)
+
+setwd('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\bean_dip_2018-2024\\field trials\\data\\clean_data')
+
+damage2019 <- read.csv('clean_damage_2019.csv') %>% 
+  mutate(pct_pucker='NA')
+damage2020 <- read.csv('clean_damage_2020.csv') %>% 
+  mutate(pct_pucker='NA')
+damage2021 <- read.csv('clean_damage_2021.csv')
+damage2022 <- read.csv('clean_damage_2022.csv') %>% 
+  select(-...18, -sampling.round) %>% 
+  mutate(pct_pucker='NA')
+
