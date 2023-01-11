b83ac6dea67ddfc5ebcc41792b9b43d560354d10
karinburghardt <62911261+karinburghardt@users.noreply.github.com>
2023-01-11 11:27:15 -0500

    new insect analysis code started

---
+++ treated vs untreated/insects.R
@@ -0,0 +1,18 @@
+################################################################################
+##  insects.R: Examining field soybean trial insect community data.
+##
+##  Authors: Karin Burghardt
+##  Date created: January 10, 2023
+################################################################################
+
+
+#Karin's path
+setwd("~/Dropbox (Smithsonian)/bean_dip_2018-2024/field trials/data/raw_data")
+
+insects <- read.csv('beanDIP_insectdata_allyears.csv')
+
+
+
+
+
+
