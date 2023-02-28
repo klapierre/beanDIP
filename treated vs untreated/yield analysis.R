e44ecb1164e9fa94b0d6a227cd6345e753eca30e
kmcgurrin <kmcgurri@umd.edu>
2023-02-28 18:04:35 -0500

    Changed data source to be clean_all_years_long.csv instead of individual metrics/years

--- treated vs untreated/yield analysis.R
+++ treated vs untreated/yield analysis.R
@@ -1,139 +1,97 @@
 #####################################################################
-## yield analysis.R
-## combining, graphing, and analyzing variety trial yield data from all years
+##  yield analysis.R
+##  graphing and analyzing variety trial yield data from all years
+##  treated/untreated pairs only
 ##
-## Author: Kelsey McGurrin 
+##  Author: Kelsey McGurrin 
 #####################################################################
 
 # working directory path (add yours if different)
 setwd("~/Dropbox/bean_dip_2018-2024/field trials/data")
 
-####setup####
+#### setup ####
 library(tidyverse)
 
-dat_19<-read_csv("clean_data/clean_harvest_2019.csv")
-dat_19<-dat_19 %>%
-  add_column(year=2019) %>%
-  select(year, site, variety, plot, indiv, plant_biomass_g)
-
-dat_20<-read_csv("clean_data/clean_harvest_2020.csv")
-dat_20<-dat_20 %>%
-  add_column(year=2020) %>%
-  select(year, site, variety, plot, indiv, plant_biomass_g)
-
-dat_21<-read_csv("clean_data/clean_harvest_2021.csv")
-dat_21<-dat_21 %>%
-  add_column(year=2021) %>%
-  select(year, site, variety, plot, indiv, plant_biomass_g)
-
-dat_22<-read_csv("clean_data/clean_harvest_2022.csv")
-dat_22<-dat_22 %>%
-  select(year, site, variety, plot, indiv, plant_biomass_g)
-
-data<-rbind(dat_19,dat_20,dat_21,dat_22)
+# cleaned data file
+data<-read_csv("clean_data/clean_all_years_long.csv")
 data$site<-factor(data$site,levels=c("K","C","W","PH"))
 
-#### put in the real variety names and whether seed treated or untreated ####
-data<-data %>% 
-  mutate(variety_name = ifelse(
-  year == 2019 & variety %in% c(67, 27),
-  'AG38X8',
-  ifelse(
-    year == 2020 & variety %in% c(2, 5),
-    'AG38X8',
-    ifelse(
-      year == 2021 & variety %in% c(19, 59),
-      'AG38X8',
-      ifelse(
-        year == 2022 & variety %in% c(73),
-        'AG38X8',
-        ifelse(
-          year == 2019 & variety == 83,
-          'SH3814LL',
-          ifelse(
-            year == 2020 & variety == 50,
-            'SH3814LL',
-            ifelse(
-              year == 2021 & variety %in% c(57, 58),
-              'SH3814LL',
-              ifelse(
-                year == 2019 & variety == 31,
-                'S39-G2X',
-                ifelse(
-                  year == 2020 & variety == 70,
-                  'S39-G2X',
-                  ifelse(
-                    year == 2021 & variety == 82,
-                    'S39-G2X',
-                    ifelse(
-                      year == 2019 & variety == 55,
-                      '7390ET',
-                      ifelse(year ==
-                               2019 & variety == 32, '539XT', 'NA')
-                    )
-                  )
-                )
-              )
-            )
-          )
-        )
-      )
-    )
-  )
-)) %>%
-  mutate(seed_coat = ifelse(
-    year == 2019 & variety == 67,
-    'untrt',
-    ifelse(
-      year == 2020 & variety == 2,
-      'untrt',
-      ifelse(
-        year == 2021 & variety %in% c(19, 57),
-        'untrt',
-        ifelse(year == 2022 &
-                 variety == 73, 'untrt', 'trt')
-      )
-    )
-  ))
 
+#### all varieties ####
 
-#### peek at everything ####
-ggplot(data, aes(x=seed_coat,y=plant_biomass_g,color=variety_name))+geom_point()+facet_wrap(~year)
+# all points
+ggplot(data,aes(x=site,y=beans_g,color=brandline_seedcoat))+
+  geom_jitter()+
+  facet_wrap(~year,nrow=1)
 
+# calculate means and error bars
+siteAvgDf<-data %>%
+  group_by(site,year,brandline_seedcoat) %>%
+  summarise(site_avg=mean(beans_g,na.rm=T),
+            se=sd(beans_g,na.rm = T)/sqrt(n()))
 
-#### subset our target variety ####
-# our target variety doesn't have treated/untreated in 2022
-# SH3814LL has treated/untreated only in 2021
+ggplot(siteAvgDf,aes(x=site,y=site_avg,group=brandline_seedcoat,color=brandline_seedcoat))+
+  geom_line(size=1.5)+
+  geom_point()+
+  geom_errorbar(aes(ymin=site_avg-se,
+                    ymax=site_avg+se),width=0.2,size=1.5)+
+  facet_wrap(~year,nrow=1)+
+  labs(title = "Average Yield (beans g)")
+
+#### AG38X8 2019-2021 ####
+
+# all points (n= 6 plants per plot for untreated, 4 plants for treated)
 AG<-data %>%
-  filter(variety_name=="AG38X8" & year != 2022)
+  filter(brandline=="AG38X8" & year== c(2019, 2020, 2021))
 
-#overlayed line graphs across environment
-#calculate means and error bars
-siteAvgDf<-AG %>%
-  group_by(site,year,variety_name,seed_coat) %>%
-  summarise(site_avg=mean(plant_biomass_g,na.rm=T),
-            se=sd(plant_biomass_g,na.rm = T)/sqrt(n()))
+ggplot(AG,aes(x=site,y=beans_g,color=seed_treat,shape=seed_treat))+
+  geom_jitter()+
+  facet_wrap(~year,nrow=1)+
+  labs(title = "AG38X8 Yield (beans g)")
 
-ggplot(siteAvgDf, aes(x=site,y=site_avg,group=seed_coat,linetype=seed_coat,color=variety_name))+geom_line(size=1.5)+geom_point(size=2)+
+
+# calculate means and error bars
+siteAvgDf<-data %>%
+  filter(brandline=="AG38X8" & year== c(2019,2020,2021)) %>%
+  group_by(site,year,brandline,seed_treat) %>%
+  summarise(site_avg=mean(beans_g,na.rm=T),
+            se=sd(beans_g,na.rm = T)/sqrt(n()))
+
+ggplot(siteAvgDf,aes(x=site,y=site_avg,group=seed_treat,linetype=seed_treat))+
+  geom_line(size=1.5)+
+  geom_point()+
   geom_errorbar(aes(ymin=site_avg-se,
                     ymax=site_avg+se),width=0.2,size=1.5)+
   facet_wrap(~year,nrow=1)+
-  ylab("Average Aboveground Biomass (g)")
+  labs(title = "AG38X8 Average Yield (beans g)")
 
-#### subset SH3814LL ####
-# SH3814LL has treated/untreated only in 2021
+
+
+#### SH3814LL 2021 ####
+
+# all points (n= 4 plants per plot)
 SH<-data %>%
-  filter(variety_name == "SH3814LL" & year == 2021)
+  filter(brandline=="SH3814 LL" & year== 2021)
 
-#overlayed line graphs across environment
-#calculate means and error bars
-siteAvgDf<-SH %>%
-  group_by(site,year,variety_name,seed_coat) %>%
-  summarise(site_avg=mean(plant_biomass_g,na.rm=T),
-            se=sd(plant_biomass_g,na.rm = T)/sqrt(n()))
+ggplot(SH,aes(x=site,y=beans_g,color=seed_treat,shape=seed_treat))+
+  geom_jitter()+
+  facet_wrap(~year,nrow=1)+
+  labs(title = "SH3814LL Yield (beans g)")
 
-ggplot(siteAvgDf, aes(x=site,y=site_avg,group=seed_coat,linetype=seed_coat,color=variety_name))+geom_line(size=1.5)+geom_point(size=2)+
+
+# calculate means and error bars
+siteAvgDf<-data %>%
+  filter(brandline=="SH3814 LL" & year== 2021) %>%
+  group_by(site,year,brandline,seed_treat) %>%
+  summarise(site_avg=mean(beans_g,na.rm=T),
+            se=sd(beans_g,na.rm = T)/sqrt(n()))
+
+ggplot(siteAvgDf,aes(x=site,y=site_avg,group=seed_treat,linetype=seed_treat))+
+  geom_line(size=1.5)+
+  geom_point()+
   geom_errorbar(aes(ymin=site_avg-se,
                     ymax=site_avg+se),width=0.2,size=1.5)+
   facet_wrap(~year,nrow=1)+
-  ylab("Average Aboveground Biomass (g)")
+  labs(title = "SH3814LL Average Yield (beans g)")
+
+
