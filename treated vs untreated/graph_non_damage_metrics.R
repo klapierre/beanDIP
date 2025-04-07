78dcd2c0e986e8fa1de421c9bb51d88094fbff8f
kmcgurrin <kmcgurri@umd.edu>
2025-04-07 10:30:12 -0400

    updated to only incorporate varieties/years where we have a treated/untreated comparison

--- treated vs untreated/graph_non_damage_metrics.R
+++ treated vs untreated/graph_non_damage_metrics.R
@@ -1,7 +1,7 @@
 ##############################################################################################
 ##
 ##  graph_non_damage_metrics.R: for any data except damage. can make line graphs with means  
-##                              and SE, scatterplot matrices, and correlation heat maps
+##                              and SE, scatterplot matrices, and correlation heat maps.
 ##
 ##  Author: Kelsey McGurrin
 ##
@@ -16,45 +16,28 @@ library(ggcorrplot)
 # working directory (add yours if different)
 setwd("~/Dropbox/bean_dip_2018-2024/field trials/data")
 
-# clean data file
+# open clean data file 
 data<-read_csv("clean_data/clean_all_years_long.csv")
 
-#### all varieties, all years ####
 
-# pick a column/metric to plot
+# filter so only varieties/years where we have a treated/untreated comparison
+janitor::tabyl(data,year,seed_treat)
 
-siteAvgDf<-data %>%
-  group_by(site,year,brandline,seed_treat) %>%
-  summarise(site_avg=mean(beans_g,na.rm=T),
-            se=sd(beans_g,na.rm = T)/sqrt(n()))
+data %>% janitor::tabyl(year,brandline_seedcoat)
 
-#not working now because of linetype?
-# ggplot(siteAvgDf, aes(x=site,y=site_avg,group=seed_treat,linetype=seed_treat,color=brandline))+
-#   geom_line(size=1.5)+
-#   geom_point(size=2)+
-#   geom_errorbar(aes(ymin=site_avg-se,
-#                     ymax=site_avg+se),width=0.2,size=1.5)+
-#   facet_wrap(~year,nrow=1)+
-#   ylab("Site Average Beans (g)")
+# should be 218 rows
+data <- data %>%
+  filter(brandline=="AG38X8" & year== c(2019,2020,2021) #122
+         | brandline=="SH3814 LL" & year== 2021 #96
+         ) 
 
-
-#### one variety across time ####
-siteAvgDf<-data %>%
-  filter(brandline_seedcoat=="AG38X8.untreated") %>%
-  group_by(site,year,brandline_seedcoat) %>%
-  summarise(site_avg=mean(beans_g,na.rm=T),
-            se=sd(beans_g,na.rm = T)/sqrt(n()))
-
-ggplot(siteAvgDf,aes(x=year,y=site_avg,group=site,color=site))+
-  geom_point()+
-  geom_line(size=1.5)+
-  geom_errorbar(aes(ymin=site_avg-se,
-                    ymax=site_avg+se),width=0.2,size=1.5)
+# so sites display properly from west to east
+data$site<-factor(data$site, levels = c("K", "C", "W", "PH"))
 
 #### treated/untreated pairs only ####
 # AG38X8 2019-2021 
 siteAvgDf<-data %>%
-  filter(brandline=="AG38X8" & year== c(2019,2020,2021)) %>%
+  filter(brandline=="AG38X8") %>%
   group_by(site,year,brandline,seed_treat) %>%
   summarise(site_avg=mean(beans_g,na.rm=T),
             se=sd(beans_g,na.rm = T)/sqrt(n()))
@@ -68,7 +51,7 @@ ggplot(siteAvgDf,aes(x=site,y=site_avg,group=seed_treat,linetype=seed_treat,colo
 
 # SH3814 2021
 siteAvgDf<-data %>%
-  filter(brandline=="SH3814 LL" & year== 2021) %>%
+  filter(brandline=="SH3814 LL") %>%
   group_by(site,year,brandline,seed_treat) %>%
   summarise(site_avg=mean(beans_g,na.rm=T),
             se=sd(beans_g,na.rm = T)/sqrt(n()))
@@ -80,55 +63,47 @@ ggplot(siteAvgDf,aes(x=site,y=site_avg,group=seed_treat,linetype=seed_treat,colo
                     ymax=site_avg+se),width=0.2,size=1.5)+
   facet_wrap(~year,nrow=1)
 
-####2021####
-#import data
-all<-read.csv("clean_data/clean_all_2021.csv")
-all$variety<-as.factor(all$variety)
-all$site<-factor(all$site, levels = c("K", "C", "W", "PH"))
-
-#boxplots
-boxes<-ggplot(all, aes(x = variety, y= beans_g))
+#### boxplots ####
+boxes<-ggplot(data, aes(x = brandline_seedcoat, y= beans_g))
 boxes+geom_boxplot()+facet_wrap(~site,nrow = 1)
 
-#scatterplots
-sp<-ggplot(all, aes(x = plant_biomass_g, y= trial_yield))
+#### scatterplots ####
+sp<-ggplot(data, aes(x = plant_biomass_g, y= est_trial_yield))
 sp+geom_point()+stat_smooth(method="lm")
-#color code by variety
-csp<-ggplot(all, aes(x = plant_biomass_g, y= trial_yield, color= site))
-csp+geom_point()
 
-#correlation of metrics
+
+#### correlation of metrics ####
 #select only metrics of interest
-subsall<-select(all,
-                plant_biomass_g,beans_g,healthy,
+subsdata<-select(data,
+                plant_biomass_g,beans_g,healthy_count,
                 root_biomass_g,
-                nodule_count,stem_diameter_cm=root_collar_diameter_cm,
-                avg_toughness,avg_trichomes,SLA,leaf_dry_matter,pressure_chamber,
-                gH.,Phi2,PhiNPQ,Relative.Chlorophyll,Thickness,trial_yield
+                nodule_count,stem_diameter_cm,
+                leaf_toughness,leaf_trichomes_count,SLA,leaf_dry_matter,lwp_bar,
+                Phi2_prop,PhiNPQ_prop,Relative.Chlorophyll,Thickness_mm,est_trial_yield
 )
 
 #heat map with correlation values
-corrdata <- round(cor(subsall,use="complete.obs"), 1)
+corrdata <- round(cor(subsdata,use="complete.obs"), 1)
 ggcorrplot(corrdata,type="lower",show.legend=F,lab=T,hc.order = T)
 
 #scatterplot matrices
 #trait data measured by hand
-pairs(~SLA+leaf_dry_matter+avg_toughness+avg_trichomes+pressure_chamber,data=subsall)
+pairs(~SLA+leaf_dry_matter+leaf_toughness+leaf_trichomes_count+lwp_bar,data=subsdata)
 #trait data from photosynq
-pairs(~PhiNPQ+Phi2+Relative.Chlorophyll+gH.+Thickness,data=subsall)
+pairs(~PhiNPQ_prop+Phi2_prop+Relative.Chlorophyll+Thickness_mm,data=subsdata)
 #harvest data
-pairs(~plant_biomass_g+beans_g+healthy+root_biomass_g+nodule_count+stem_diameter_cm,data=subsall)
+pairs(~plant_biomass_g+beans_g+healthy_count+root_biomass_g+nodule_count+stem_diameter_cm,data=subsdata)
 
-#line graphs across environment
+#### line graphs across environment ####
 #calculate means and error bars
-siteAvgDf<-all %>%
-  group_by(site,variety) %>%
-  summarise(site_avg_trichomes=mean(avg_trichomes,na.rm=T),
-            se_avg_trichomes=sd(avg_trichomes,na.rm = T)/sqrt(n()),
+siteAvgDf<-data %>%
+  group_by(site,brandline_seedcoat) %>%
+  summarise(site_avg_trichomes=mean(leaf_trichomes_count,na.rm=T),
+            se_avg_trichomes=sd(leaf_trichomes_count,na.rm = T)/sqrt(n()),
             site_Relative.Chlorophyll=mean(Relative.Chlorophyll,na.rm=T),
             se_Relative.Chlorophyll=sd(Relative.Chlorophyll,na.rm=T)/sqrt(n()),
-            site_Phi2=mean(Phi2,na.rm=T),
-            se_Phi2=sd(Phi2,na.rm=T)/sqrt(n()),
+            site_Phi2=mean(Phi2_prop,na.rm=T),
+            se_Phi2=sd(Phi2_prop,na.rm=T)/sqrt(n()),
             site_SLA=mean(SLA,na.rm=T),
             se_SLA=sd(SLA,na.rm = T)/sqrt(n()),
             site_beans_g=mean(beans_g,na.rm=T),
@@ -139,16 +114,17 @@ siteAvgDf<-all %>%
             se_root_biomass_g=sd(root_biomass_g,na.rm = T)/sqrt(n()),
             site_RtoS=mean(root_biomass_g/plant_biomass_g,na.rm=T),
             se_RtoS=sd((root_biomass_g/plant_biomass_g),na.rm=T/sqrt(n())),
-            site_trial_yield=mean(trial_yield,na.rm=T),
-            se_trial_yield=sd(trial_yield,na.rm = T)/sqrt(n())
+            site_trial_yield=mean(est_trial_yield,na.rm=T),
+            se_trial_yield=sd(est_trial_yield,na.rm = T)/sqrt(n())
   )
 #graph with individual facets
-facets<-ggplot(siteAvgDf, aes(x=site,y=site_trial_yield,colour=variety,group=variety))+facet_wrap(~variety,nrow=1)+geom_point()+geom_line()+
+facets<-ggplot(siteAvgDf, aes(x=site,y=site_trial_yield,colour=brandline_seedcoat,group=brandline_seedcoat))+
+  facet_wrap(~brandline_seedcoat,nrow=1)+geom_point()+geom_line()+
   geom_errorbar(aes(ymin=site_trial_yield-se_trial_yield,
                     ymax=site_trial_yield+se_trial_yield),width=0.2)
 facets
 #overlayed all together
-overlay<-ggplot(siteAvgDf, aes(x=site,y=site_trial_yield,colour=variety,group=variety))+geom_line(size=1.5)+
+overlay<-ggplot(siteAvgDf, aes(x=site,y=site_trial_yield,colour=brandline_seedcoat,group=brandline_seedcoat))+geom_line(size=1.5)+
   geom_errorbar(aes(ymin=site_trial_yield-se_trial_yield,
                     ymax=site_trial_yield+se_trial_yield),width=0.2,size=1.5)
 overlay
